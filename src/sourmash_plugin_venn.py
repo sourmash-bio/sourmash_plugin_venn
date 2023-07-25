7ddb5e8b3f9cf1915b52d9df5ae73b7253d21fbe
C. Titus Brown <titus@idyll.org>
2023-07-25 15:25:07 -0400

    upgraded command line args

--- src/sourmash_plugin_venn.py
+++ src/sourmash_plugin_venn.py
@@ -1,9 +1,11 @@
-"Read/write sketches via plugin."
+"Create and write out a pairwise Venn diagram."
+import sys
+
 import pylab
-from matplotlib_venn import venn2, venn3
+from matplotlib_venn import venn2
 
 import sourmash
-from sourmash.logging import debug_literal
+from sourmash.logging import debug_literal, error, notify
 from sourmash.plugins import CommandLinePlugin
 
 
@@ -21,29 +23,43 @@ class Command_Venn(CommandLinePlugin):
         super().__init__(subparser)
         # add argparse arguments here.
         debug_literal('RUNNING cmd venn __init__')
-        subparser.add_argument('sketches', nargs='+')
-        subparser.add_argument('-o', '--output', default=None)
+        subparser.add_argument('sketches', nargs='+',
+                               help="file(s) containing exactly two sketches")
+        subparser.add_argument('-o', '--output', default=None,
+                               help="save Venn diagram image to this file",
+                               required=True)
+        subparser.add_argument('--name1', default=None,
+                               help="override name for first sketch")
+        subparser.add_argument('--name2', default=None,
+                               help="override name for second sketch")
 
     def main(self, args):
         # code that we actually run.
         super().main(args)
-        print('RUNNING cmd', self, args)
+        debug_literal(f'RUNNING cmd {self} {args}')
 
         sketch_files = list(args.sketches)
-        print(sketch_files)
 
         sketches = []
         for filename in sketch_files:
+            notify(f"Loading sketches from {filename}")
             x = list(sourmash.load_file_as_signatures(filename))
+            notify(f"...loaded {len(x)} sketches from {filename}.")
             sketches.extend(x)
 
-        assert len(sketches) == 2
+        if len(sketches) < 2:
+            error("ERROR: only found one sketch. Must supply exactly 2.")
+            sys.exit(-1)
+        elif len(sketches) > 2:
+            error("ERROR: found more than one sketch. Must supply exactly 2.")
+            sys.exit(-1)
 
-        hashes1 = set(sketches[0].minhash.hashes)
-        hashes2 = set(sketches[1].minhash.hashes)
-
-        label1 = sketches[0].name
-        label2 = sketches[1].name
+        sketch1, sketch2 = sketches
+        hashes1 = set(sketch1.minhash.hashes)
+        hashes2 = set(sketch2.minhash.hashes)
+            
+        label1 = args.name1 or sketches[0].name
+        label2 = args.name2 or sketches[1].name
 
         venn2([hashes1, hashes2], set_labels=(label1, label2))
         if args.output:
