467ed8747a051e8900277bb258ba9f34ac92c54c
Tessa Pierce Ward <bluegenes@users.noreply.github.com>
2023-07-30 06:27:19 -0400

    [MRG] Add capacity to use three sketches (#2)
    * add venn3
    
    * import venn3

--- src/sourmash_plugin_venn.py
+++ src/sourmash_plugin_venn.py
@@ -2,7 +2,7 @@
 import sys
 
 import pylab
-from matplotlib_venn import venn2
+from matplotlib_venn import venn2, venn3
 
 import sourmash
 from sourmash import sourmash_args
@@ -18,14 +18,14 @@ from sourmash.cli.utils import (add_ksize_arg, add_moltype_args)
 
 class Command_Venn(CommandLinePlugin):
     command = 'venn'
-    description = "makes a venn diagram of the overlap between two sketches"
+    description = "makes a venn diagram of the overlap between two or three sketches"
 
     def __init__(self, subparser):
         super().__init__(subparser)
         # add argparse arguments here.
         debug_literal('RUNNING cmd venn __init__')
         subparser.add_argument('sketches', nargs='+',
-                               help="file(s) containing exactly two sketches")
+                               help="file(s) containing two or three sketches")
         subparser.add_argument('-o', '--output', default=None,
                                help="save Venn diagram image to this file",
                                required=True)
@@ -33,6 +33,8 @@ class Command_Venn(CommandLinePlugin):
                                help="override name for first sketch")
         subparser.add_argument('--name2', default=None,
                                help="override name for second sketch")
+        subparser.add_argument('--name3', default=None,
+                               help="override name for (optional) third sketch")
         add_ksize_arg(subparser)
         add_moltype_args(subparser)
 
@@ -55,23 +57,29 @@ class Command_Venn(CommandLinePlugin):
             sketches.extend(x)
 
         if not len(sketches):
-            error("ERROR: no sketches found. Must supply exactly 2.")
+            error("ERROR: no sketches found. Must supply 2 or 3.")
             sys.exit(-1)
         elif len(sketches) == 1:
-            error("ERROR: only found one sketch. Must supply exactly 2.")
+            error("ERROR: only found one sketch. Must supply 2 or 3.")
             sys.exit(-1)
-        elif len(sketches) > 2:
-            error("ERROR: found more than one sketch. Must supply exactly 2.")
+        elif len(sketches) > 3:
+            error("ERROR: found more than three sketches. Must supply 2 or 3.")
             sys.exit(-1)
 
-        sketch1, sketch2 = sketches
-        hashes1 = set(sketch1.minhash.hashes)
-        hashes2 = set(sketch2.minhash.hashes)
+        hashes1 = set(sketches[0].minhash.hashes)
+        hashes2 = set(sketches[1].minhash.hashes)
             
         label1 = args.name1 or sketches[0].name
         label2 = args.name2 or sketches[1].name
 
-        venn2([hashes1, hashes2], set_labels=(label1, label2))
+        if len(sketches) == 2:
+            venn2([hashes1, hashes2], set_labels=(label1, label2))
+
+        elif len(sketches) == 3:
+            hashes3 = set(sketches[2].minhash.hashes)
+            label3 = args.name3 or sketches[2].name
+            venn3([hashes1, hashes2,hashes3], set_labels=(label1, label2, label3))
+
         if args.output:
             print(f"saving to '{args.output}'")
             pylab.savefig(args.output)
